7b551e2c08142b8edb18d995e73d39d088913fb7
hugosenari <hugosenari@gmail.com>
2022-09-03 19:17:15 -0300

    chore: re index pvim

---
+++ source/l3wqxdqx7rx90cnmc4r7x40w2q0rms4y-source/pvim.nimble
@@ -0,0 +1,24 @@
+# Package
+
+version       = "0.18.3"
+author        = "oakes"
+description   = "A parasitic editor"
+license       = "Public Domain"
+srcDir        = "src"
+installExt    = @[
+  when defined(windows):
+    "dll"
+  elif defined(macosx):
+    "dylib"
+  elif defined(linux):
+    "so"
+]
+bin           = @["pvim"]
+
+task dev, "Run dev version":
+  exec "nimble run pvim"
+
+# Dependencies
+
+requires "nim >= 1.2.6"
+requires "paravim >= 0.18.3"
---
+++ source/src/pvim.nim
@@ -0,0 +1,71 @@
+import paranim/glfw
+from paranim/gl import nil
+from paravim import nil
+from paravim/terminal import nil
+from os import nil
+from strutils import nil
+
+proc runGui(params: seq[string]) =
+  doAssert glfwInit()
+
+  glfwWindowHint(GLFWContextVersionMajor, 3)
+  glfwWindowHint(GLFWContextVersionMinor, 3)
+  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
+  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
+  glfwWindowHint(GLFWResizable, GLFW_TRUE)
+  glfwWindowHint(GLFWTransparentFramebuffer, GLFW_TRUE)
+
+  let w: GLFWWindow = glfwCreateWindowC(1024, 768, "Paravim", nil, nil)
+  if w == nil:
+    quit(-1)
+
+  w.makeContextCurrent()
+  glfwSwapInterval(1)
+
+  discard w.setKeyCallback(paravim.keyCallback)
+  discard w.setCharCallback(paravim.charCallback)
+  discard w.setMouseButtonCallback(paravim.mouseButtonCallback)
+  discard w.setCursorPosCallback(paravim.cursorPosCallback)
+  discard w.setFramebufferSizeCallback(paravim.frameSizeCallback)
+  discard w.setScrollCallback(paravim.scrollCallback)
+
+  w.setCursor(glfwCreateStandardCursor(GLFW_IBEAM_CURSOR))
+
+  var game = gl.RootGame()
+  paravim.init(game, w, params)
+
+  while not w.windowShouldClose:
+    let mustPoll = paravim.tick(game, true)
+    w.swapBuffers()
+    if mustPoll:
+      glfwPollEvents()
+    else:
+      glfwWaitEvents()
+
+  w.destroyWindow()
+  glfwTerminate()
+
+proc runTerminal(params: seq[string]) =
+  terminal.init(params)
+  while true:
+    terminal.tick()
+    os.sleep(10)
+
+when isMainModule:
+  var
+    params = newSeq[string]()
+    guiMode = true
+  for param in os.commandLineParams():
+    if strutils.startsWith(param, "-"):
+      case param:
+        of "--terminal", "-t":
+          guiMode = false
+        else:
+          quit("Unrecognized parameter: " & param)
+    else:
+      params.add(param)
+
+  if guiMode:
+    runGui(params)
+  else:
+    runTerminal(params)
